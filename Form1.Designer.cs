8cc3d02c9bb3640139e117d50fa9cc5b0f64e8c6
G <xandir1993@gmail.com>
2024-02-25 00:34:49 +0300

    progressBar added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,12 +33,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // textBox1
@@ -84,59 +82,27 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Инфо о видео:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 204);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Хотите его скачать?";
-            this.label3.Visible = false;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(159, 199);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Да";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Visible = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(240, 199);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Нет";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Visible = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(45, 247);
+            this.textBox2.Location = new System.Drawing.Point(42, 205);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(270, 20);
+            this.textBox2.Size = new System.Drawing.Size(357, 20);
             this.textBox2.TabIndex = 8;
             this.textBox2.Visible = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(42, 231);
+            this.label4.Location = new System.Drawing.Point(39, 189);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(161, 13);
+            this.label4.Size = new System.Drawing.Size(310, 13);
             this.label4.TabIndex = 9;
-            this.label4.Text = "Укажите расположение ниже:";
+            this.label4.Text = "Укажите расположение, куда хотите сохранить файл ниже:";
             this.label4.Visible = false;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(338, 245);
+            this.button4.Location = new System.Drawing.Point(405, 202);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 10;
@@ -145,24 +111,29 @@
             this.button4.Visible = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(42, 233);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(438, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 279);
+            this.ClientSize = new System.Drawing.Size(526, 268);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Youtube Video Downloader";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,12 +146,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using AngleSharp.Text;
+using MediaFileProcessor.Models.Video;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -8,19 +9,26 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using YoutubeExplode;
 using YoutubeExplode.Converter;
 using YoutubeExplode.Videos.Streams;
 using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace YoutubeDownloader
 {
+
     public partial class Form1 : Form
     {
         string link;
+        string fullSavePath;
+        bool error;
 
         public Form1()
         {
@@ -31,79 +39,117 @@ namespace YoutubeDownloader
         private void button1_Click(object sender, EventArgs e)
         {
             link = textBox1.Text;
-            listBox1.Update();
+            listBox1.Items.Clear();
+            
+
             if (!String.IsNullOrEmpty(link))
             {
+                error = false;
                 ShowInfo(link);
-                label3.Visible = true;
-                button2.Visible = true;
-                button3.Visible = true;
-                button4.Visible = true;
             }
             else 
             {
                 MessageBox.Show("Вы ничего не ввели");
             }
 
+
+
             async void ShowInfo(string str)
             {
-                var youtube = new YoutubeClient();
-                var videoUrl = str;
-                var video = await youtube.Videos.GetAsync(videoUrl);
+                try
+                {
+                    var youtube = new YoutubeClient();
+                    var videoUrl = str;
+                    var video = await youtube.Videos.GetAsync(videoUrl);
 
-                var title = video.Title; // название видео
-                var author = video.Author.ChannelTitle; // название канала
-                var duration = video.Duration; // продолжительность
-                var ID = video.Id;
+                    var title = video.Title; // название видео
+                    var author = video.Author.ChannelTitle; // название канала
+                    var duration = video.Duration; // продолжительность
 
-                listBox1.Items.Add($"Название видео: {title}");
-                listBox1.Items.Add($"Автор: {author}");
-                listBox1.Items.Add($"Продолжительность: {duration}");
-                listBox1.Items.Add($"ID: {ID}");
+                    listBox1.Items.Add($"Название видео: {title}");
+                    listBox1.Items.Add($"Автор: {author}");
+                    listBox1.Items.Add($"Продолжительность: {duration}");
+
+                    label4.Visible = true;
+                    textBox2.Visible = true;
+                    button4.Visible = true;
+                }
+                catch (Exception)
+                {
+                    error = true;
+                    MessageBox.Show("Это не ссылка на Youtube или в ней содержатся русские буквы");
+                }
             }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            label4.Visible = true;
-            textBox2.Visible = true;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             var savePath = textBox2.Text;
 
-            if (Directory.Exists(savePath))
+            if (Directory.Exists(savePath) && error == false)
             {
                 DownloadVideo(link, savePath);
+                MessageBox.Show("Начал загружать видео");
             }
             else
             {
                 MessageBox.Show("Вы ничего не ввели или такое расположение не существует!");
             }
 
+
             async void DownloadVideo(string link, string path)
             {
-                var youtube = new YoutubeClient();
-
-                var video = await youtube.Videos.GetAsync(link);
-                var forbiddenSymbols = new string[] {"\\", "/", ":", "*", "?", "\"", "<", ">"};
-                string name = video.Title;
-                foreach (var s in forbiddenSymbols)
+                try
                 {
-                    name = name.Replace(s, "");
-                }
+                    var youtube = new YoutubeClient();
 
-                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link); //получаем список доступных видео
-                var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality(); //берем видео наилучшего качества              
-                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{path}\\{name}.{streamInfo.Container}"); //сохраняем по пути + имя файла из описания
+                    var video = await youtube.Videos.GetAsync(link);
+                    var forbiddenSymbols = new string[] { "\\", "/", ":", "*", "?", "\"", "<", ">", "|" };
+                    string name = video.Title;
+                    foreach (var s in forbiddenSymbols)
+                    {
+                        name = name.Replace(s, "");
+                    }
+                    //var progressHandler = new Progress<double>();
+                    var progress = new Progress<double>(p =>
+                    {
+                        progressBar1.Value = Convert.ToInt32(p * 100);
+                        progressBar1.Text = Convert.ToString(p * 100) + "%";
+                    });
+
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link); //получаем список доступных видео
+                    var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality(); //берем видео наилучшего качества            
+                    fullSavePath = $"{path}\\{name}.{streamInfo.Container}"; //прокидываем в глобальную переменную полное ия файла
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, fullSavePath, progress); //сохраняем по пути + имя файла из описания
+
+                    
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Изначальная ссылка на видео не верна");
+                    error = true;
+                }
             }
         }
-
-        private void button3_Click(object sender, EventArgs e)
+        async void CheckDone()
         {
-            MessageBox.Show("Жаль");
-            Process.GetCurrentProcess().Kill();
+            string dir = Path.GetDirectoryName(fullSavePath);
+            string file = Path.GetFileName(fullSavePath);
+
+            Type shellAppType = Type.GetTypeFromProgID("Shell.Application");
+            dynamic shell = Activator.CreateInstance(shellAppType);
+            dynamic folder = shell.NameSpace(dir);
+            dynamic folderItem = folder.ParseName(file);
+            string value = folder.GetDetailsOf(folderItem, 27).ToString();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(2000);
+                if (int.Parse(value) > 0)
+                    MessageBox.Show("Скачивание завершено");
+                else
+                    MessageBox.Show("Скачивание завершено с ошибкой!");
+            }
         }
     }
 }
